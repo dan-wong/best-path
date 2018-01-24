10b01907c68872080420a1e5baac206c07ce07b3
Daniel <dan_wong@outlook.com>
2018-01-24 20:20:31 +1300

    Added timing to tests

--- src/test/java/AStarTest.java
+++ src/test/java/AStarTest.java
@@ -1,13 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class AStarTest {
-	public AStarTest() {
+	private long _startTime;
+
+	public AStarTest() {}
+
+	@Before
+	public void setup() {
+		_startTime = System.nanoTime();
+	}
+
+	@After
+	public void after() {
+		System.out.println((System.nanoTime() - _startTime)/1000000 + "ms");
 	}
 
 	@Test
 	public void testAStar_BestGreedyPathFunction() {
+		System.out.println("testAStar_BestGreedyPathFunction()");
 		int[][] rawGraph = {
 				{3, 6, 3, 7, 2, 5},
 				{1, 4, 2, 4, 1, 4},
@@ -22,5 +36,28 @@ public class AStarTest {
 				"[4,2]\n" +
 				"[5,1]\n" +
 				"[6,2]", bestPath.getBestPath());
+	}
+
+	//2162ms - 30,30,30
+	@Test
+	public void testAStar_withRandomGraph() {
+		System.out.println("testAStar_withRandomGraph()");
+		int[][] rawGraph = generateGraph(30, 30, 30);
+
+		BestPath bestPath = new BestPath(rawGraph);
+		bestPath.getBestPath();
+	}
+
+	private int[][] generateGraph(int width, int length, int maxValue) {
+		int[][] graph = new int[width][length];
+
+		for (int i=0; i<width; i++) {
+			for (int j=0; j<length; j++) {
+				graph[i][j] = (int) (Math.random() * maxValue) + 1;
+			}
+		}
+
+		System.out.println("Random Graph Generated");
+		return graph;
 	}
 }
